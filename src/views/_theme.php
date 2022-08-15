23357f57228a8b5bdebeb9f6363aa8ab18ed0c9a
gabrieldsvr <gabrieldsvrcontato@gmail.com>
2022-08-15 19:36:27 -0300

    testando meta description

--- src/views/_theme.php
+++ src/views/_theme.php
@@ -5,8 +5,11 @@
     <meta name="description" content="Fabrx">
     <meta name="keywords" content="Fabrx">
     <meta name="author" content="Fabrx">
+    <meta name="description"
+          content="Trabalhamos com criação, desenvolvimento e designer de websites de forma profissional garantindo os melhores preços e prazos de confecção graças a nossa renomada equipe que possui décadas de experiências na área de programação e desenvolvimento"/>
+
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    <title><?=$title?></title>
+    <title><?= $title ?></title>
     <link href="<?= url("assets/images/favicon.ico") ?>" rel="shortcut icon" type="image/x-icon">
     <!-- Fabrx External CSS -->
     <link href="<?= url("assets/css/loading-bar.min.css") ?>" rel="stylesheet" type="text/css" media="all">
@@ -23,10 +26,17 @@
     <script src='https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.js'></script>
     <link href='https://api.mapbox.com/mapbox-gl-js/v2.8.2/mapbox-gl.css' rel='stylesheet'/>
 
-    <link href="<?= url("assets/plugin/font-awesome/css/font-awesome.min.css") ?>" rel="stylesheet" type="text/css" media="all">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/brands.min.css" integrity="sha512-OivR4OdSsE1onDm/i3J3Hpsm5GmOVvr9r49K3jJ0dnsxVzZgaOJ5MfxEAxCyGrzWozL9uJGKz6un3A7L+redIQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/fontawesome.min.css" integrity="sha512-xX2rYBFJSj86W54Fyv1de80DWBq7zYLn2z0I9bIhQG+rxIF6XVJUpdGnsNHWRa6AvP89vtFupEPDP8eZAtu9qA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="<?= url("assets/plugin/font-awesome/css/font-awesome.min.css") ?>" rel="stylesheet" type="text/css"
+          media="all">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/brands.min.css"
+          integrity="sha512-OivR4OdSsE1onDm/i3J3Hpsm5GmOVvr9r49K3jJ0dnsxVzZgaOJ5MfxEAxCyGrzWozL9uJGKz6un3A7L+redIQ=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/fontawesome.min.css"
+          integrity="sha512-xX2rYBFJSj86W54Fyv1de80DWBq7zYLn2z0I9bIhQG+rxIF6XVJUpdGnsNHWRa6AvP89vtFupEPDP8eZAtu9qA=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
 
     <link href="<?= url("assets/css/estilo.css") ?>" rel="stylesheet" type="text/css" media="all">
 
@@ -67,7 +77,8 @@
 <script src="<?= url("assets/js/prism.min.js") ?>"></script>
 <script src="<?= url("assets/js/charts-light.js") ?>"></script>
 <script src="<?= url("assets/js/fabrx-scripts.js") ?>"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
+        crossorigin="anonymous"></script>
 
 <script src="<?= url("assets/css/jquery-toast-plugin/jquery.toast.min.js") ?>"></script>
 
