fde24fa00552a502e377d171cb383062fa327c80
gabrieldsvr <gabrieldsvrcontato@gmail.com>
2022-11-10 09:51:13 -0300

    mudando a metatag

--- src/views/_theme.php
+++ src/views/_theme.php
@@ -13,17 +13,17 @@
 
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
-    <meta property="og:url" content="https://metatags.io/">
+    <meta property="og:url" content="https://starkhouse.com.br/">
     <meta property="og:title" content="Starkhouse">
     <meta property="og:description" content="Imóveis que encantam em Florianópolis">
-    <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+    <meta property="og:image" content="https://starkhouse.com.br/src/assets/images/logo/logo_color.png">
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image">
-    <meta property="twitter:url" content="https://metatags.io/">
+    <meta property="twitter:url" content="https://starkhouse.com.br/">
     <meta property="twitter:title" content="Starkhouse">
     <meta property="twitter:description" content="Imóveis que encantam em Florianópolis">
-    <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+    <meta property="twitter:image" content="https://starkhouse.com.br/src/assets/images/logo/logo_color.png">
 
 
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
